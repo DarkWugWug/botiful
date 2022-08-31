218407f119c36650448dfb26f3cce2ece4422983
“DarkWugWug” <nolanrosen15@gmail.com>
2022-08-31 00:04:20 -0400

    Packaged

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,9 +1,10 @@
-import { TextBasedChannel } from 'discord.js';
+import { Client, TextBasedChannel } from 'discord.js';
 export declare class Formatter {
     private readonly prefix;
     private readonly adminRole;
+    private readonly client;
     static substitutions: Record<string, (self: Formatter) => string>;
-    constructor(prefix: string, adminRole: string);
+    constructor(prefix: string, adminRole: string, client: Client);
     fmt(x: string): string;
 }
 export declare function doTyping(channel: TextBasedChannel, typing?: number): Promise<void>;
